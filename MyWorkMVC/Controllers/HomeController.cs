3eddc8971dc922df78e97e567b15a4b5ec2537ae
Micah-Bello <micahbello16@gmail.com>
2021-07-29 20:34:42 +0100

    Added custom layout page and supporting css and js files

--- MyWorkMVC/Controllers/HomeController.cs
+++ MyWorkMVC/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace MyWorkMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- MyWorkMVC/wwwroot/js/site.js
+++ MyWorkMVC/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
