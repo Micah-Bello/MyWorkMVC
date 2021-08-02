ccd410673db6cb51302997f3cc359693980c4bf3
Micah-Bello <micahbello16@gmail.com>
2021-08-02 21:19:42 +0100

    Added js code for hourly rate and service fee calculations

--- MyWorkMVC/wwwroot/js/site.js
+++ MyWorkMVC/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+function updateFields(val, id) {
+    var serviceFee = 0;
+    var actualRate = 0;
+    var hourlyRate = 0;
+
+    if (id === 'hourlyRateInput') {
+        actualRate = val * 0.8;
+        hourlyRate = Number(val);
+    }
+    else {
+        hourlyRate = val / 0.8;
+        actualRate = Number(val);
+    }
+
+    serviceFee = actualRate - hourlyRate;
+
+
+    document.getElementById('serviceFeeLabel').innerText = serviceFee.toFixed(2);
+    document.getElementById('actualRateInput').value = actualRate.toFixed(2);
+    document.getElementById('hourlyRateInput').value = hourlyRate.toFixed(2);
+}
